a93d3941ee395bbc9694b7f085391a36f7ff3998
AnaNs <anaclarans@unifei.edu.br>
2021-04-12 14:16:05 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,374 @@
+//Declaração das cartas do jogo
+
+var carta1 = {
+    nome:"Gandalf o cinzento",
+    imagem:"https://i2.wp.com/manualdosgames.com/wp-content/uploads/2019/05/Gandalf-1170x770-1170x770.jpg" ,
+    atributos: 
+    {resistenciaAnel: 3,
+    ataque: 56,
+    magica: 19,}
+}
+
+var carta2 = {
+    nome:"Peregrin Took",
+    imagem:"http://pm1.narvii.com/6263/19f8fd6dd5cb7e20761a803c4d83aa7559a09f00_00.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 35,
+    magica: 1,}
+}
+
+var carta3 = {
+    nome:"Frodo Bolseiro",
+    imagem:"http://2.bp.blogspot.com/-5byHT0yZWZw/UlNZTwXh7MI/AAAAAAAAL1w/YGCm5HO_eWo/s1600/FrodoEspada.jpg" ,
+    atributos:
+    {resistenciaAnel: 3,
+    ataque: 28,
+    magica: 4,}
+}
+
+var carta4 = {
+    nome:"Aragorn",
+    imagem:"https://conteudo.imguol.com.br/c/entretenimento/dd/2018/05/17/viggo-mortensen-como-aragorn-na-trilogia-o-senhor-dos-aneis-1526603479172_v2_1500x1101.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 59,
+    magica: 10,}
+}
+
+var carta5 = {
+    nome:"Arwen",
+    imagem:"https://i.pinimg.com/originals/bb/d8/d6/bbd8d6775021da52013697a9164f6f7a.png",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 3,
+    magica: 14,}
+}
+
+var carta6 = {
+    nome:"Samwise Gamgee",
+    imagem:"https://i.pinimg.com/originals/d2/d2/83/d2d28345d9a5f3f5e1d098212224b3a9.jpg",
+    atributos:
+    {resistenciaAnel: 3,
+    ataque: 40,
+    magica: 1,}
+}
+
+var carta7 = {
+    nome:"Legolas",
+    imagem:"https://conversamos.files.wordpress.com/2013/03/legolas.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 42,
+    magica: 14,}
+}
+
+var carta8 = {
+    nome:"Boromir",
+    imagem:"https://i.pinimg.com/originals/d2/ee/96/d2ee96cd905f8639d75018b6863d29bb.jpg",
+    atributos:
+    {resistenciaAnel: 1,
+    ataque: 52,
+    magica: 0,}
+}
+
+var carta9 = {
+    nome:"Bilbo Bolseiro",
+    imagem:"https://cdn.folhape.com.br/upload/dn_arquivo/2020/06/ian-holm-2_1.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 7,
+    magica: 5,}
+}
+
+var carta10 = {
+    nome:"Sauron",
+    imagem:"https://quintacapa.com.br/wp-content/uploads/2019/08/SauronTheBrightLordMissionImage-destaque.png",
+    atributos:
+    {resistenciaAnel: 5,
+    ataque: 45,
+    magica: 20,}
+}
+
+var carta11 = {
+    nome:"Saruman o Branco",
+    imagem:"https://i.pinimg.com/originals/a3/b7/a7/a3b7a7fab3ff446687c9e72156079953.jpg",
+    atributos:
+    {resistenciaAnel: 0,
+    ataque: 49,
+    magica: 19.5,}
+}
+
+var carta12 = {
+    nome:"O Balrog",
+    imagem:"https://i.pinimg.com/originals/6f/0e/21/6f0e2103d5e221e10f15f67be24ac563.jpg",
+    atributos:
+    {resistenciaAnel: 3,
+    ataque: 70,
+    magica: 19,}
+}
+
+var carta13 = {
+    nome:"Guerreiro de gondor",
+    imagem:"https://i.pinimg.com/originals/04/2d/cf/042dcfce20ecf90d5e66be4d9b404bbe.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 49,
+    magica: 0,}
+}
+
+var carta14 = {
+    nome:"Orc de Isengard",
+    imagem:"https://pbs.twimg.com/media/D1FL6wYWoAIpQpV.jpg",
+    atributos:
+    {resistenciaAnel: 1,
+    ataque: 56,
+    magica: 0,}
+}
+
+var carta15 = {
+    nome:"Isildur",
+    imagem:"http://pm1.narvii.com/6308/abdb42d960f125fda2154ec6c2b8a002604937c7_00.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 59,
+    magica: 8,}
+}
+
+var carta16 = {
+    nome:"Elrond",
+    imagem:"https://i.pinimg.com/474x/97/1b/99/971b99dd76120f33dc053827bd81433d.jpg",
+    atributos:
+    {resistenciaAnel: 3,
+    ataque: 38,
+    magica: 19,}
+}
+
+var carta17 = {
+    nome:"Gimli",
+    imagem:"https://i.pinimg.com/originals/2b/ed/c9/2bedc9d9106bee5e7c3f5f018019d0f6.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 49,
+    magica: 1,}
+}
+
+var carta18 = {
+    nome:"Haldir",
+    imagem:"https://i.pinimg.com/originals/09/74/11/0974114e4fbe9bf9f0f501f054508ed2.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 45,
+    magica: 12,}
+}
+
+var carta19 = {
+    nome:"Éomer",
+    imagem:"http://2.bp.blogspot.com/_PunV3wX0-Qg/TAZYjWgY1wI/AAAAAAAAAvA/1GT2WxEoGXU/s1600/Eomer.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 56,
+    magica: 1,}
+}
+
+var carta20 = {
+    nome:"Meriadoc Brandebuque",
+    imagem:"https://i.pinimg.com/originals/1e/53/ee/1e53eeea02819531453539bdf3b1d16f.jpg",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 39,
+    magica: 1,}
+}
+
+var carta21 = {
+    nome:"Barbárvore",
+    imagem:"http://pm1.narvii.com/6367/2e089ba19072b72d50f5a041caa4e17ec152bd81_00.jpg",
+    atributos:
+    {resistenciaAnel: 2.5,
+    ataque: 15,
+    magica: 17,}
+}
+
+var carta22 = {
+    nome:"Nazgûl",
+    imagem:"http://pm1.narvii.com/6794/c00673c8a0e46e1e130b1a2d82f3cb9c592ae712v2_00.jpg",
+    atributos:
+    {resistenciaAnel: 1,
+    ataque: 69,
+    magica: 14,}
+}
+
+var carta23 = {
+    nome:"Faramir",
+    imagem:"https://i.pinimg.com/originals/ba/f9/c1/baf9c1ad500a6eb0b52b0b88e220411e.png",
+    atributos:
+    {resistenciaAnel: 2,
+    ataque: 52,
+    magica: 4,}
+}
+
+var carta24 = {
+    nome:"Gollum",
+    imagem:"https://wp-content.bluebus.com.br/wp-content/uploads/2015/12/03234609/the-hobbit-gollum-660x440.jpg",
+    atributos:
+    {resistenciaAnel: 0,
+
+    ataque: 47,
+    magica: 4,}
+}
+
+var carta25 = {
+    nome:"Orc de Moria",
+    imagem:"https://www.zbrushcentral.com/uploads/default/original/4X/f/1/e/f1e714059f6af71a1d6a00a70a471ae060315557.jpeg",
+    atributos:
+    {resistenciaAnel: 1,
+    ataque: 49,
+    magica: 0,}
+}
+
+//Declaração do array cartas, onde os elementos são as cartas anteriormente declaradas
+var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18, carta19, carta20, carta21, carta22, carta23, carta24, carta25];
+var cartaMaquina;
+var cartaJogador;
+var pontosJogador = 0;
+var pontosMaquina = 0;
+
+atualizaPlacar();
+atualizaCartas();
+
+
+//Sortea aleatoriamente o índice que definirá a carta do jogador e da máquina, e retira a carta selecionada do vetor, impedindo que ambos peguem a mesma carta
+function sortearCarta() {
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador, 1)
+
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
+
+    document.getElementById('btSortear').disabled = true
+    document.getElementById('btJogar').disabled = false
+
+    exibeCartaJogador()
+}
+
+//Imprime a carta do jogador na tela 
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador")
+    var moldura = '<img src="https://www.pngkit.com/png/full/326-3269374_best-photos-of-game-card-template-board-game.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+//Identifica e retorna qual atributo o jogador selecionou no botão de opção
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+//Compara o atributo do jogador e da máquina e adiciona um ponto para o ganhador
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu</p>'
+        pontosJogador++
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu</p>'
+        pontosMaquina++
+    } else {
+        htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }
+
+    //Habilita o botão "Próxima Rodada" e desabilita o "jogar" 
+    document.getElementById('btProximaRodada').disabled = false
+    divResultado.innerHTML = htmlResultado
+    document.getElementById('btJogar').disabled = true
+    
+    //Chamada das funções que atualizam o placar, exibem a carta do oponente e atualizam o número de cartas
+    atualizaPlacar()
+    exibeCartaMaquina()
+    atualizaQuantidadeDeCartas()
+}
+
+//Imprime a carta da máquina na tela 
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://www.pngkit.com/png/full/326-3269374_best-photos-of-game-card-template-board-game.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        console.log(atributo)
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status --spacing'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+//Limpa as cartas e o resultado anteriormente impressos na tela e habilita o botão "sortear"
+function proximaRodada() {
+
+    //verifica se há cartas disponíveis para uma nova rodada
+    if (cartas.length == 1) {
+        alert("Fim de jogo, as cartas acabaram.")
+        if( pontosJogador > pontosMaquina)
+        {
+            var htmlResultado = '<p class="resultado-final">Venceu</p>'
+        }
+        else if ( pontosMaquina > pontosJogador)
+        {
+            var htmlResultado = '<p class="resultado-final">Perdeu</p>'
+        }
+        else
+        {
+            var htmlResultado = '<p class="resultado-final">Empatou</p>'
+        }
+    }
+    else {
+    var divCartas = document.getElementById('cartas')
+
+    divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`
+
+    document.getElementById('btSortear').disabled = false
+    document.getElementById('btJogar').disabled = true
+    document.getElementById('btProximaRodada').disabled = true
+
+    var divResultado = document.getElementById('resultado')
+    divResultado.innerHTML = ""
+    }
+}
+
+//Atualiza e imprime na tela a quantidade de cartas
+function atualizaQuantidadeDeCartas() {
+    var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+    var html = "Quantidade de cartas no jogo: " + cartas.length
+
+    divQuantidadeCartas.innerHTML = html
+}
+
+//Atualiza e imprime na tela o placas
+function atualizaPlacar() {
+    var divPlacar = document.getElementById('placar')
+    var html = "Jogador " + pontosJogador + " | " + pontosMaquina + " Máquina"
+
+    divPlacar.innerHTML = html
+}
